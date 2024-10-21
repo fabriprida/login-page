915f55768d6cca182efeed3e57d2b9a01817e78f
Fabrizio Prida <pridafabrizio@gmail.com>
2024-10-21 16:16:37 -0300

    updating URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
-const MONGO_URI='mongodb+srv://pridafabrizio:QvKBBZU2hm2TGNxZ@cluster0.xlzt0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 export const connectDB = async () => {
     try {
